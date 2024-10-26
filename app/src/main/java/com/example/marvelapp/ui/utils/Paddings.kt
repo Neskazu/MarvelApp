1878a2561ec98eca2471ebc89f308ca6b534de01
Neskazu <erikfil1995@gmail.com>
2024-10-26 09:04:52 +0600

    HeroListScreen Refactoring
    Take out HeroItem from HeroListScreen
    Take out BackgroundTriangles from HeroListScreen
    Created Padding.kt for card padding
    Created UiUtils.kt for screen height and width
    Created Sizes.kt for card sizes
    Take out text from HeroListScreen

---
+++ app/src/main/java/com/example/marvelapp/ui/utils/Paddings.kt
@@ -0,0 +1,6 @@
+package com.example.marvelapp.ui.utils
+import androidx.compose.ui.unit.dp
+
+object Paddings {
+    val CardPadding = 16.dp
+}
---
+++ app/src/main/java/com/example/marvelapp/ui/utils/Sizes.kt
@@ -0,0 +1,6 @@
+package com.example.marvelapp.ui.utils
+
+object Sizes {
+    val CardWidthFactor = 0.85f
+    val CardHeightFactor = 0.7f
+}
